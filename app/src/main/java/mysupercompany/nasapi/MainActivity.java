dff17197307f89947c4cd417c4119c10884f381c
46066294 <46066294p@iespoblenou.org>
2017-01-09 06:52:01 +0100

    Mejora de layouts. Se añade fondo principal

--- app/src/main/java/mysupercompany/nasapi/MainActivity.java
+++ app/src/main/java/mysupercompany/nasapi/MainActivity.java
@@ -1,9 +1,7 @@
 package mysupercompany.nasapi;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
